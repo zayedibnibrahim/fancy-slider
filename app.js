aea10a67ac4e7316ea663611da50ba1698c9459d
zayedibnibrahim <zayed.ibn@gmail.com>
2021-02-14 19:50:28 +0600

    Till it slide

--- app.js
+++ app.js
@@ -4,14 +4,25 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
+
+searchBtn.addEventListener('click', function () {
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search').value;
+  const Key = '15674931-a9d714b6e9d654524df198e00&q';
+  getImages(Key, search);
+  sliders.length = 0;
+})
+
+const getImages = (apiKey, query) => {
+  fetch(`https://pixabay.com/api/?key=${apiKey}=${query}&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+    .catch(err => console.log(err))
+}
+
 // selected image 
 let sliders = [];
-
-
-// If this key doesn't work
-// Find the name in the url and go to their website
-// to create your own api key
-const KEY = '20271125-75b0a41125c43e8215ab75640';
 
 // show images 
 const showImages = (images) => {
@@ -28,32 +39,31 @@ const showImages = (images) => {
 
 }
 
-const getImages = (query) => {
-  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
-    .then(response => response.json())
-    .then(data => showImages(data.hitS))
-    .catch(err => console.log(err))
-}
-
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
-  } else {
-    alert('Hey, Already added !')
+  }else {
+    sliders.pop(element);
   }
 }
-var timer
+// speed selector
+sliderBtn.addEventListener('click', function () {
+  createSlider();
+})
+
+var timer;
 const createSlider = () => {
   // check slider image length
   if (sliders.length < 2) {
     alert('Select at least 2 image.')
     return;
   }
+
   // crate slider previous next area
   sliderContainer.innerHTML = '';
   const prevNext = document.createElement('div');
@@ -108,15 +118,3 @@ const changeSlide = (index) => {
 
   items[index].style.display = "block"
 }
-
-searchBtn.addEventListener('click', function () {
-  document.querySelector('.main').style.display = 'none';
-  clearInterval(timer);
-  const search = document.getElementById('search');
-  getImages(search.value)
-  sliders.length = 0;
-})
-
-sliderBtn.addEventListener('click', function () {
-  createSlider()
-})
